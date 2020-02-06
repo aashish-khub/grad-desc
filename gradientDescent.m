84d009e7a5662de569d5482c7072f9708ffdf5e2
Aashish Khubchandani <40303727+aashish-khub@users.noreply.github.com>
2020-02-06 15:10:05 -0500

    Add files via upload
    MATLAB files for Introductory Machine Learning

---
+++ gradientDescent.m
@@ -0,0 +1,42 @@
+function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
+%GRADIENTDESCENT Performs gradient descent to learn theta
+%   theta = GRADIENTDESCENT(X, y, theta, alpha, num_iters) updates theta by 
+%   taking num_iters gradient steps with learning rate alpha
+
+% Initialize some useful values
+m = length(y); % number of training examples
+J_history = zeros(num_iters, 1);
+
+for iter = 1:num_iters
+
+    % ====================== YOUR CODE HERE ======================
+    % Instructions: Perform a single gradient step on the parameter vector
+    %               theta. 
+    %
+    % Hint: While debugging, it can be useful to print out the values
+    %       of the cost function (computeCost) and gradient here.
+    %
+    
+    %delta_theta = zeros(size(theta));
+    hX = X*theta;
+    errorThisIter = hX - y;
+    for j = 1:(length(theta)),
+        s = 0;
+        thisJx = X(:,j);
+        for i = 1:m,
+            s += (hX(i) - y(i))*thisJx(i);
+        end;
+        theta(j) -= (alpha/m) * s;
+        
+        %delta_theta(j) += errorThisIter' * thisJx;  %'I have no idea why this failed to work but whatever
+
+    %theta -= (alpha/m) * delta_theta;
+
+    % ============================================================
+
+    % Save the cost J in every iteration    
+    J_history(iter) = computeCost(X, y, theta);
+
+    end
+
+end
